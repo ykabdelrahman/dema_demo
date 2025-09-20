da53c0325881197a0c8ff7e62f2d1c90192c8a8f
Abdelrahman Hussein <ykabdelrahmanhussein@gmail.com>
2025-09-20 19:19:21 +0300

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2531135903"
+    serviceWorkerVersion: "502380747"
   }
 });
